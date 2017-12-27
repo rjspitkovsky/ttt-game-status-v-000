1d4969fb747e75dc878cbb136d988395a334b7aa
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-27 19:09:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,100 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+]
+
+def won?(board)
+
+  WIN_COMBINATIONS.find do |win_combination|
+    board[win_combination[0]] == board[win_combination[1]] &&
+    board[win_combination[0]] == board[win_combination[2]] #&&
+  #  position_taken?(board, win_combination[0])
+  end
+
+#  WIN_COMBINATIONS.each do |win_combination|
+#    win_index_1 = win_combination[0]
+#    win_index_2 = win_combination[1]
+#    win_index_3 = win_combination[2]
+
+#    position_1 = board[win_index_1]
+#    position_2 = board[win_index_2]
+#    position_3 = board[win_index_3]
+
+
+
+#    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+#      return win_combination
+#    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+#      return win_combination
+#    else
+#      false
+#    end
+#  end
+
+end
+
+
+def full?(board)
+  board.none? do |i|
+    i == " " || i.nil?
+  end
+
+  #test_array = []
+  #x = 0
+  #while x < board.length
+  #  if board[x] == "X" || board[x] == "O"
+  #    test_array << board[x]
+  #  end
+  #  x += 1
+  #end
+  #if test_array.length == board.length
+  #  return true
+  #else
+  #  false
+  #end
+end
+
+def draw?(board)
+  won?(board) == nil && full?(board) == true
+
+  #if full?(board) == true && won?(board) == false
+  #  return true
+  #else
+  #  false
+  #end
+end
+
+def over?(board)
+  draw?(board) == true || won?(board) != nil
+
+
+#  if won?(board) == true
+#    true
+#  elsif full?(board) == true
+#    true
+#  else
+#    false
+# end
+end
+
+
+def winner(board)
+  if won?(board) != nil
+    winner = board[won?(board)[0]]
+  else
+    nil
+  end
+
+#  if won?(board) == false
+#    return nil
+#  end
+end
