8ea342d92bbe10eef60e1b6a913cc7d14f21247d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-27 19:19:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,7 @@ def won?(board)
    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
      return win_combination
    elsif position_1 == " " && position_2 == " " && position_3 == " "
-     return false 
+      false 
    else
      false
    end
