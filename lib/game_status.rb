ef54ad536a8a757881cdd7a0710dec47096abab3
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-27 19:40:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -78,7 +78,7 @@ def draw?(board)
 end
 
 def over?(board)
-  draw?(board) == true || (won?(board) == true && full?(board) == true)
+  draw?(board) == true || won?(board) != nil
 
 
 #  if won?(board) == true
