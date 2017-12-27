ab1ad02f4388c08d9afef5969df4d3a68c58967e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-27 19:58:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ def won?(board)
 
   WIN_COMBINATIONS.detect do |win_combination|
     board[win_combination[0]] == board[win_combination[1]] &&
-    board[win_combination[0]] == board[win_combination[2]] &&
+    board[win_combination[1]] == board[win_combination[2]] &&
     position_taken?(board, win_combination[0])
   end
 
